dd5502d04b837eaecf6484e06a05a7aa44337952
Jan Gerardi <jan_gerardi@outlook.com>
2025-08-21 15:20:35 +0200

    header optimized

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  isMenuOpen = false;
+
+  openMenu() {
+    this.isMenuOpen = true;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  onMenuItemClick() {
+    this.closeMenu();
+  }
 }
