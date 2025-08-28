21df050605643f46aa195ec4bf6515c629f534ee
Jan Gerardi <jan_gerardi@outlook.com>
2025-08-28 21:06:38 +0200

    skills optimized and added overlay

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -19,5 +19,4 @@ export class AboutMeComponent {
   hideOverlay() {
     this.isOverlayOpen = false;
   }
-
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SkillListComponent } from './skill-list/skill-list.component';
 
 @Component({
   selector: 'app-skills',
-  imports: [SkillListComponent],
+  imports: [SkillListComponent, CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  
+  isOverlayOpen = false;
 
+  showOverlay() {
+    this.isOverlayOpen = true;
+  }
+
+  hideOverlay() {
+    this.isOverlayOpen = false;
+  }
 }
