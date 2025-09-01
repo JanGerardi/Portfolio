20d66516eb36cc6f3b121e8d4814b57b012b318c
Jan Gerardi <jan_gerardi@outlook.com>
2025-09-01 15:43:32 +0200

    portfolio added overlays

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,6 +10,7 @@ import { Component } from '@angular/core';
 export class PortfolioComponent {
 
   currentIndex = 0;
+  isOverlayOpen = false;
 
   projects = [
     {
@@ -17,6 +18,7 @@ export class PortfolioComponent {
       image: "./assets/image/join.png",
       skills: "HTML | CSS | Firebase | Angular | TypeScript",
       description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      development: "In the Join project, I was responsible for coordinating the team and leading daily meetings. At the same time, I contributed equally to the programming and consistently provided helpful and solution-oriented support when challenges arose.",
       github: "https://github.com/JanGerardi/join",
       live: ""
     },
@@ -25,6 +27,7 @@ export class PortfolioComponent {
       image: "./assets/image/el_pollo_loco.png",
       skills: "HTML | CSS | JavaScript",
       description: "A 2D platformer in the spirit of Super Mario, reimagined with a lively Mexican theme – from sombreros to mariachi music.",
+      development: "As part of the Jump'n'Run project, I was solely responsible for the concept, planning, and implementation. I structured the development process, defined clear milestones, and consistently kept track of the overall progress.",
       github: "https://github.com/JanGerardi/el-pollo-loco",
       live: ""
     }
@@ -36,5 +39,13 @@ export class PortfolioComponent {
 
   nextProject() {
     this.currentIndex = (this.currentIndex + 1) % this.projects.length;
+  }
+
+  showOverlay() {
+    this.isOverlayOpen = true;
+  }
+
+  hideOverlay() {
+    this.isOverlayOpen = false;
   }
 }
