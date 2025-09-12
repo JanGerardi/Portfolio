6c52e0809786312d6e8d7b3dce07449a909c2275
Jan Gerardi <jan_gerardi@outlook.com>
2025-09-12 12:12:30 +0200

    about-me added translation and design optimizations

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateDirective, TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateDirective, TranslatePipe],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss',
 })
