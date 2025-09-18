a955fc6dcaeac74c2fc904a61c99e6267f6147a3
Jan Gerardi <jan_gerardi@outlook.com>
2025-09-18 12:29:56 +0200

    contact design optimizations, imprint and privacy-policy added content restriction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { TranslateService, TranslatePipe, TranslateDirective } from '@ngx-translate/core';
+import { filter } from 'rxjs/operators';
 
 
 @Component({
@@ -14,9 +15,19 @@ import { TranslateService, TranslatePipe, TranslateDirective } from '@ngx-transl
 export class AppComponent {
   title = 'Portfolio Jan Gerardi';
 
-  constructor(private translate: TranslateService){
+  constructor(private translate: TranslateService, private router: Router){
     this.translate.addLangs(['de', 'en']);
     this.translate.setDefaultLang('en');
     this.translate.use('en');
+
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const scrollToTopPaths = ['/imprint', '/privacy-policy'];
+
+        if (scrollToTopPaths.includes(event.urlAfterRedirects)) {
+          window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+        }
+      });
   }
 }
